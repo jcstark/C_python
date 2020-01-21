d5c19282393f7b3e18faefa2ae0fe9cf6e489b2b
jcstark <jcstark@users.noreply.github.com>
2020-01-21 18:41:58 +0000

    automated commit by check50 [check50=True]

--- caesar.py
+++ caesar.py
@@ -6,7 +6,7 @@ from cs50 import get_string
 def main():
     # Ensure correct usage
     if len(argv) != 2 or int(argv[1]) < 0: #or # ins'tnum:
-        print("Usage: python caesar.py k or ./caesar.py k")
+        #print("Usage: python caesar.py k or ./caesar.py k")
         return 1
     k = int(argv[1]) % 26
     # Insert text to encrypt
