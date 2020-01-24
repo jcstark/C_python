aad648ba78bb87b3e83cd5234df1e51fb3e702e4
jcstark <jcstark@users.noreply.github.com>
2020-01-24 13:03:57 +0000

    automated commit by check50 [check50=True]

--- plurality.c
+++ plurality.c
@@ -99,7 +99,7 @@ void print_winner(void)
     {
         for (int j = i + 1; j < candidate_count; j++)
         {
-            if (candidates[i].votes > candidates[j].votes)
+            if (candidates[i].votes >= candidates[j].votes)
             {
                 winner = candidates[i].name;
                 w = i;
